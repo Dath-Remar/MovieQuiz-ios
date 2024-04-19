a3e6d8f9e51f1f4a2d554b4ac412271575fb8af9
Dath-Remar <sergei-balak@yandex.ru>
2024-04-19 23:38:52 +0300

    форматирование презентера

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -1,9 +1,12 @@
 import UIKit
 import Reachability
 
+// MARK: - MovieQuizPresenter Declaration
+
 final class MovieQuizPresenter: QuestionFactoryDelegate {
     
     // MARK: - Properties
+    
     var questionFactory: QuestionFactoryProtocol?
     private var currentQuestionIndex = 0
     let questionsAmount: Int = 10
@@ -11,17 +14,16 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
     var correctAnswers = 0
     weak var viewController: MovieQuizViewController?
     var statisticService: StatisticService?
+    private let reachability = try! Reachability()
     
     // MARK: - Initializers
+    
     init(statisticService: StatisticService?) {
         self.questionFactory = QuestionFactory(delegate: self)
         self.statisticService = statisticService
     }
     
-    // MARK: - Public Methods
-    
-    // Глобальная переменная для доступа к Reachability
-    let reachability = try! Reachability()
+    // MARK: - Reachability
 
     func isConnectedToNetwork() -> Bool {
         reachability.whenReachable = { _ in
@@ -34,93 +36,13 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         do {
             try reachability.startNotifier()
         } catch {
-            print("Unable to start notifier")
+            print("Не удалось запустить уведомления")
         }
 
         return reachability.connection != .unavailable
     }
 
-    func handleDataLoadingError(with error: Error) {
-        // Подготовка модели алерта с замыканием для сброса квиза и повторной загрузки данных
-        let alertModel = AlertModel(
-            title: "Ошибка",
-            message: error.localizedDescription,
-            buttonText: "Попробовать еще раз"
-        ) { [weak self] in
-            guard let self = self else { return }
-            self.viewController?.hideLoadingIndicator()  // Управление UI через ссылку на ViewController
-            self.viewController?.resetQuiz()  // Сброс индексов и счётчиков квиза
-            self.viewController?.showLoadingIndicator()  // Управление UI через ссылку на ViewController
-
-            // Повторная попытка загрузки данных с задержкой
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {  // Задержка перед повторной попыткой загрузки данных
-                self.attemptToReloadData()
-            }
-        }
-        // Показ алерта через ViewController
-        viewController?.alertPresenter.showAlert(model: alertModel)
-    }
-
-    private func attemptToReloadData() {
-        if !isConnectedToNetwork() {
-            handleNoInternetConnection()
-            return
-        }
-
-        self.questionFactory?.loadData(completion: { [weak self] in
-            self?.viewController?.showCurrentQuestion()  // Показ текущего вопроса после перезагрузки данных
-        })
-    }
-
-    private func handleNoInternetConnection() {
-        let alertModel = AlertModel(
-            title: "Нет соединения",
-            message: "Подключение к Интернету отсутствует. Пожалуйста, проверьте ваше соединение и попробуйте снова.",
-            buttonText: "OK",
-            completion: {}
-        )
-        viewController?.alertPresenter.showAlert(model: alertModel)
-    }
-
-    
-    func getStatisticsText() -> String {
-        guard let statisticsService = statisticService else {
-            return "Статистика недоступна"
-        }
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
-        let gamesPlayed = statisticsService.gamesCount
-        let bestGameScore = "\(statisticsService.bestGame.correct)/\(statisticsService.bestGame.total)"
-        let bestGameDate = dateFormatter.string(from: statisticsService.bestGame.date)
-        let overallAccuracy = String(format: "%.2f%%", statisticsService.totalAccuracy * 100)
-        return """
-        Количество сыгранных квизов: \(gamesPlayed)
-        Рекорд: \(bestGameScore) (\(bestGameDate))
-        Средняя точность: \(overallAccuracy)
-        """
-    }
-    
-    func startQuiz() {
-        questionFactory?.loadData(completion: {
-            self.viewController?.showCurrentQuestion()
-        })
-    }
-    
-    func resetQuestionIndex() {
-        currentQuestionIndex = 0
-    }
-    
-    func requestNextQuestion() {
-        questionFactory?.requestNextQuestion()
-    }
-    
-    func handleYesButtonClicked() {
-        processAnswer(true)
-    }
-    
-    func handleNoButtonClicked() {
-        processAnswer(false)
-    }
+    // MARK: - QuestionFactoryDelegate Methods
     
     func didReceiveQuestion(question: QuizQuestion?) {
         DispatchQueue.main.async { [weak self] in
@@ -136,18 +58,124 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
             })
         }
     }
-    
-    func didLoadDataFromServer() {
-        viewController?.hideLoadingIndicator()
-    }
-    
+
     func didFailToLoadData(with error: Error) {
         viewController?.handleDataLoadingError(with: error)
     }
-    
+
+    func didLoadDataFromServer() {
+        viewController?.hideLoadingIndicator()
+    }
+
     func didReceiveError(error: Error) {
         viewController?.handleDataLoadingError(with: error)
     }
+
+    // MARK: - Error Handling Methods
+    
+    func handleDataLoadingError(with error: Error) {
+        let alertModel = AlertModel(
+            title: "Ошибка",
+            message: error.localizedDescription,
+            buttonText: "Попробовать еще раз"
+        ) { [weak self] in
+            guard let self = self else { return }
+            self.viewController?.hideLoadingIndicator()
+            self.viewController?.resetQuiz()
+            self.viewController?.showLoadingIndicator()
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.attemptToReloadData()
+            }
+        }
+        viewController?.alertPresenter.showAlert(model: alertModel)
+    }
+
+    private func attemptToReloadData() {
+        if !isConnectedToNetwork() {
+            handleNoInternetConnection()
+            return
+        }
+        self.questionFactory?.loadData(completion: { [weak self] in
+            self?.viewController?.showCurrentQuestion()
+        })
+    }
+
+    private func handleNoInternetConnection() {
+        let alertModel = AlertModel(
+            title: "Нет соединения",
+            message: "Подключение к Интернету отсутствует. Пожалуйста, проверьте ваше соединение и попробуйте снова.",
+            buttonText: "OK",
+            completion: {}
+        )
+        viewController?.alertPresenter.showAlert(model: alertModel)
+    }
+
+    // MARK: - Quiz Management Methods
+    
+    func startQuiz() {
+        questionFactory?.loadData(completion: {
+            self.viewController?.showCurrentQuestion()
+        })
+    }
+
+    func resetQuestionIndex() {
+        currentQuestionIndex = 0
+    }
+
+    func requestNextQuestion() {
+        questionFactory?.requestNextQuestion()
+    }
+
+    // MARK: - Response Handling Methods
+    
+    func handleYesButtonClicked() {
+        processAnswer(true)
+    }
+
+    func handleNoButtonClicked() {
+        processAnswer(false)
+    }
+
+    private func processAnswer(_ answer: Bool) {
+        guard let currentQuestion = currentQuestion else { return }
+        let isCorrect = answer == currentQuestion.correctAnswer
+        if isCorrect {
+            correctAnswers += 1
+        }
+        showAnswerResult(isCorrect: isCorrect)
+    }
+
+    private func showAnswerResult(isCorrect: Bool) {
+        viewController?.updateUIForAnswer(isCorrect: isCorrect)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.viewController?.changeStateButtons(isEnabled: true)
+            if self.isLastQuestion() {
+                self.showFinalResult()
+            } else {
+                self.switchToNextQuestion()
+                self.viewController?.showCurrentQuestion()
+            }
+        }
+    }
+
+    private func isLastQuestion() -> Bool {
+        return currentQuestionIndex == questionsAmount - 1
+    }
+
+    private func switchToNextQuestion() {
+        currentQuestionIndex += 1
+    }
+
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(data: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)"
+        )
+    }
+
+    // MARK: - Final Results Presentation
     
     func showFinalResult() {
         guard let viewController = viewController else { return }
@@ -167,50 +195,23 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         
         viewController.alertPresenter.showAlert(model: model)
     }
+
+    // MARK: - Statistical Methods
     
-    // MARK: - Private Methods
-    
-    private func processAnswer(_ answer: Bool) {
-        guard viewController?.yesButton.isEnabled ?? false, viewController?.noButton.isEnabled ?? false else { return }
-        guard let currentQuestion = currentQuestion else {
-            return
+    func getStatisticsText() -> String {
+        guard let statisticsService = statisticService else {
+            return "Статистика недоступна"
         }
-        let isCorrect = answer == currentQuestion.correctAnswer
-        if isCorrect {
-            correctAnswers += 1
-        }
-        showAnswerResult(isCorrect: isCorrect)
-    }
-    
-    private func showAnswerResult(isCorrect: Bool) {
-        viewController?.updateUIForAnswer(isCorrect: isCorrect)  // Обновляем UI в зависимости от ответа
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-            guard let self = self else { return }
-            self.viewController?.changeStateButtons(isEnabled: true) // Включаем кнопки после задержки
-            if self.isLastQuestion() {
-                self.showFinalResult()  // Показываем финальный результат, если это последний вопрос
-            } else {
-                self.switchToNextQuestion()  // Переключаем на следующий вопрос
-                self.viewController?.showCurrentQuestion()  // Показываем текущий вопрос
-            }
-        }
-    }
-    
-    private func isLastQuestion() -> Bool {
-        return currentQuestionIndex == questionsAmount - 1
-    }
-    
-    
-    private func switchToNextQuestion() {
-        currentQuestionIndex += 1
-    }
-    
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        return QuizStepViewModel(
-            image: UIImage(data: model.image) ?? UIImage(),
-            question: model.text,
-            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)"
-        )
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
+        let gamesPlayed = statisticsService.gamesCount
+        let bestGameScore = "\(statisticsService.bestGame.correct)/\(statisticsService.bestGame.total)"
+        let bestGameDate = dateFormatter.string(from: statisticsService.bestGame.date)
+        let overallAccuracy = String(format: "%.2f%%", statisticsService.totalAccuracy * 100)
+        return """
+        Количество сыгранных викторин: \(gamesPlayed)
+        Рекорд: \(bestGameScore) (\(bestGameDate))
+        Средняя точность: \(overallAccuracy)
+        """
     }
 }
